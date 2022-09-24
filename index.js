fb5b9a9fa34eb00a492b6aa64f923a9202441ee3
BrycenLikesCoding <93868711+BrycenLikesCoding@users.noreply.github.com>
2022-09-23 19:42:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,4 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname});
 });
 
-app.get('/projects', (req, res) => {
-  res.sendFile('projects.html', {root: __dirname});
-});
-
 app.listen(8080);
