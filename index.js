8f267c519d9ff3818439128b335bf95031340cbb
BrycenLikesCoding <93868711+BrycenLikesCoding@users.noreply.github.com>
2022-11-23 17:51:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: __dirname});
-});
+// hi
